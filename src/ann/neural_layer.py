10ac779c5bb8dc7fb96f0bec923980008355230b
Gv <ravigowthamaan@gmail.com>
2024-03-14 14:38:14 +0530

    Q3-backprop:initiated

---
+++ src/ann/neural_layer.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+# Represents a layer (hidden or output) in the neural network.
+class NeuralLayer:
+    
+    # Init the layer
+    def __init__(self, n_input, n_neurons, activation=None, weights=None, bias=None):
+        self.activation = activation
+        self.weights = weights if weights is not None else np.random.randn(n_input, n_neurons)
+        self.bias = bias if bias is not None else np.random.randn(n_neurons)
+        self.last_activation = None
+        self.error = None
+        self.delta = None
+    
+    # Activate the neural layer
+    def activate(self, x):
+        r = np.dot(x, self.weights) + self.bias
+        self.last_activation = self._apply_activation(r)
+        return self.last_activation
+
+    # Activation function
+    def _apply_activation(self, r):
+        if self.activation == 'sigmoid':
+            return 1 / (1 + np.exp(-r))
+        elif self.activation == 'tanh':
+            return np.tanh(r)
+        elif self.activation == 'relu':
+            return r if r>0 else 0
+        return r
+
+    # Derivative of the Activation function (will be used in backpropagation)
+    def apply_activation_derivative(self, r):
+        if self.activation == 'sigmoid':
+            return r * (1 - r)
+        elif self.activation == 'tanh':
+            return (1 - r**2)
+        elif self.activation == 'relu':
+            return 1 if r>0 else 0
+        return r
